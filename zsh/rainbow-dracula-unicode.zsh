8d2d644b6ec4faaaf00a926686df837e1b0966d6
Werner Stein <werner.stein@gmail.com>
2021-01-09 13:27:21 +0100

    add treatment for "$COLORTERM" = "truecolor"

--- zsh/rainbow-dracula-unicode.zsh
+++ zsh/rainbow-dracula-unicode.zsh
@@ -20,14 +20,26 @@
 'builtin' 'setopt' 'no_aliases' 'no_sh_glob' 'brace_expand'
 
 () {
-  typeset COLOR0_BLACK=0 #21222c
-  typeset COLOR1_RED=1 #ff5555
-  typeset COLOR2_GREEN=2 #50fa7b
-  typeset COLOR3_YELLOW=3 #f1fa8c
-  typeset COLOR4_BLUE=4 #bd93f9
-  typeset COLOR5_MAGENTA=5 #ff79c6
-  typeset COLOR6_CYAN=6 #8be9fd
-  typeset COLOR7_WHITE=7 #f8f8f2
+  # https://jonasjacek.github.io/colors/
+  if [ "$COLORTERM" = "truecolor" ]; then
+    typeset COLOR0_BLACK='#212222'
+    typeset COLOR1_RED='#ff5555'
+    typeset COLOR2_GREEN='#50fa7b'
+    typeset COLOR3_YELLOW='#f1fa8c'
+    typeset COLOR4_BLUE='#bd93f9'
+    typeset COLOR5_MAGENTA='#ff79c6'
+    typeset COLOR6_CYAN='#8be9fd'
+    typeset COLOR7_WHITE='#f8f8f2'
+  else
+    typeset COLOR0_BLACK=235
+    typeset COLOR1_RED=203
+    typeset COLOR2_GREEN=84
+    typeset COLOR3_YELLOW=228
+    typeset COLOR4_BLUE=141
+    typeset COLOR5_MAGENTA=212
+    typeset COLOR6_CYAN=117
+    typeset COLOR7_WHITE=255
+  fi
 
   emulate -L zsh -o extended_glob
 
